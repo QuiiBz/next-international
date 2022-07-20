5726364313064196a591620d9e9600db1e208648
QuiiBz <tom.lienrd@gmail.com>
2022-07-20 18:36:09 +0200

    feat: add scopes

--- examples/next/locales/en.ts
+++ examples/next/locales/en.ts
@@ -4,4 +4,8 @@ export default {
   hello: 'Hello',
   welcome: 'Hello {name}!',
   'about.you': 'Hello {name}! You have {age} yo',
+  'scope.test': 'A scope',
+  'scope.more.test': 'A scope',
+  'scope.more.param': 'A scope with {param}',
+  'scope.more.and.more.test': 'A scope',
 } as const;
--- packages/next-international/src/i18n/create-use-i18n.ts
+++ packages/next-international/src/i18n/create-use-i18n.ts
@@ -1,5 +1,5 @@
 import React, { useContext, Context } from 'react';
-import { Locale, LocaleContext, LocaleKeys, LocaleValue, Params, ParamsObject } from '../types';
+import { Locale, LocaleContext, LocaleKeys, LocaleValue, Params, ParamsObject, ScopedValue, Scopes } from '../types';
 
 export function createUsei18n<LocaleType extends Locale>(I18nContext: Context<LocaleContext<LocaleType> | null>) {
   return function useI18n() {
@@ -9,24 +9,43 @@ export function createUsei18n<LocaleType extends Locale>(I18nContext: Context<Lo
       throw new Error('`useI18n` must be used inside `I18nProvider`');
     }
 
-    return function t<Key extends LocaleKeys<LocaleType>, Value extends LocaleValue = LocaleType[Key]>(
-      key: Key,
-      ...params: Params<Value>['length'] extends 0 ? [] : [ParamsObject<Value>]
-    ) {
-      const { localeContent } = context;
+    function createT<Scope extends Scopes<LocaleType> | undefined>(scope: Scope) {
+      return function t<
+        Key extends LocaleKeys<LocaleType, Scope>,
+        Value extends LocaleValue = ScopedValue<LocaleType, Scope, Key>,
+      >(key: Key, ...params: Params<Value>['length'] extends 0 ? [] : [ParamsObject<Value>]) {
+        const { localeContent } = context as LocaleContext<LocaleType>;
 
-      let value = (localeContent?.[key] || key).toString();
-      const paramObject = params[0];
+        let value;
 
-      if (!paramObject) {
+        if (scope) {
+          console.log(scope, key);
+          value = (localeContent[`${scope}.${key}`] || key).toString();
+        } else {
+          value = (localeContent[key] || key).toString();
+        }
+
+        const paramObject = params[0];
+
+        if (!paramObject) {
+          return value;
+        }
+
+        for (const [param, paramValue] of Object.entries(paramObject as Locale)) {
+          value = value.toString().replace(`{${param}}`, paramValue.toString());
+        }
+
         return value;
-      }
+      };
+    }
 
-      for (const [param, paramValue] of Object.entries(paramObject as Locale)) {
-        value = value.toString().replace(`{${param}}`, paramValue.toString());
-      }
+    function scopedT<Scope extends Scopes<LocaleType>>(scope: Scope) {
+      return createT(scope);
+    }
 
-      return value;
+    return {
+      t: createT(undefined),
+      scopedT,
     };
   };
 }
