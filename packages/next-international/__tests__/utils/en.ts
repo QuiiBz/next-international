5b167064e4e5fff196d1ff9030b68a56159ef749
QuiiBz <tom.lienrd@gmail.com>
2022-07-24 09:22:50 +0200

    test: add useI18n test

--- packages/next-international/__tests__/utils/en.ts
+++ packages/next-international/__tests__/utils/en.ts
@@ -1,3 +1,11 @@
 export default {
   hello: 'Hello',
+  'hello.world': 'Hello World!',
+  weather: "Today's weather is {weather}",
+  'user.description': '{name} is {years} years old',
+  'namespace.hello': 'Hello',
+  'namespace.subnamespace.hello': 'Hello',
+  'namespace.subnamespace.hello.world': 'Hello World!',
+  'namespace.subnamespace.weather': "Today's weather is {weather}",
+  'namespace.subnamespace.user.description': '{name} is {years} years old',
 } as const;
