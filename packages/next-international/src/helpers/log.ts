c868a92a10daac0f75f442a8e27778026fb00b85
QuiiBz <tom.lienrd@gmail.com>
2022-07-25 11:02:52 +0200

    fix: log() returns null

--- packages/next-international/src/helpers/log.ts
+++ packages/next-international/src/helpers/log.ts
@@ -2,12 +2,9 @@ function log(type: keyof Pick<typeof console, 'error' | 'warn'>, message: string
   if (process.env.NODE_ENV !== 'production') {
     console[type](`[next-international] ${message}`);
   }
+
+  return null;
 }
 
-export function warn(message: string) {
-  log('warn', message);
-}
-
-export function error(message: string) {
-  log('error', message);
-}
+export const warn = (message: string) => log('warn', message);
+export const error = (message: string) => log('error', message);
