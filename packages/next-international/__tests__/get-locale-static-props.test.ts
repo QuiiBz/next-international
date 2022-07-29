9462de61cb1d87b33c7d54a4079defa63568dd11
QuiiBz <tom.lienrd@gmail.com>
2022-07-29 13:03:22 +0200

    test: add test for not defined locale within getLocaleStaticProps

--- packages/next-international/__tests__/get-locale-static-props.test.ts
+++ packages/next-international/__tests__/get-locale-static-props.test.ts
@@ -1,20 +1,28 @@
-import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest';
+import { describe, expect, it, vi } from 'vitest';
 import { createI18n } from '../src';
 import en from './utils/en';
 
-beforeEach(() => {
-  vi.mock('next/router', () => ({
-    useRouter: vi.fn().mockImplementation(() => ({
-      locale: 'en',
-    })),
-  }));
-});
-
-afterEach(() => {
-  vi.clearAllMocks();
-});
-
 describe('getLocaleStaticProps', () => {
+  it('should error if locale is not defined', async () => {
+    const spy = vi.spyOn(console, 'error').mockImplementation(() => null);
+    const { getLocaleStaticProps } = createI18n<typeof import('./utils/en').default>({
+      en: () => import('./utils/en'),
+      fr: () => import('./utils/fr'),
+    });
+
+    const props = await getLocaleStaticProps()({
+      locales: ['en', 'fr'],
+    });
+
+    expect(props).toEqual({
+      props: {},
+    });
+    expect(console.error).toHaveBeenCalledWith(
+      "[next-international] 'i18n.defaultLocale' not defined in 'next.config.js'",
+    );
+    spy.mockReset();
+  });
+
   it('should return default locale', async () => {
     const { getLocaleStaticProps } = createI18n<typeof import('./utils/en').default>({
       en: () => import('./utils/en'),
