47350075a74a5e2c4729fb792e20e99d6f24b222
Tim Raderschad <tim.raderschad@gmail.com>
2022-08-19 09:12:03 +0200

    feat(next-internation): use RegExp with global flag to replace all

--- packages/next-international/src/i18n/create-use-i18n.ts
+++ packages/next-international/src/i18n/create-use-i18n.ts
@@ -37,7 +37,7 @@ export function createUsei18n<Locale extends BaseLocale>(I18nContext: Context<Lo
         }
 
         for (const [param, paramValue] of Object.entries(paramObject as Locale)) {
-          value = value.toString().replaceAll(`{${param}}`, paramValue.toString());
+          value = value.toString().replace(new RegExp(`{${param}}`, 'g'), paramValue.toString());
         }
 
         return value;
