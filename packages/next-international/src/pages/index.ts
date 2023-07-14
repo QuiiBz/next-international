8e3318594e65bf20c5c3d854e5e3168fe39edb86
QuiiBz <tom.lienrd@gmail.com>
2023-07-14 16:46:33 +0200

    fix: errors

--- packages/next-international/src/pages/index.ts
+++ packages/next-international/src/pages/index.ts
@@ -12,13 +12,12 @@ import { createUseCurrentLocale } from './create-use-current-locale';
 export function createI18n<Locales extends ImportedLocales, OtherLocales extends ExplicitLocales | null = null>(
   locales: Locales,
 ) {
-  // type Locale = OtherLocales extends ExplicitLocales ? GetLocaleType<OtherLocales> : GetLocaleType<Locales>;
-
   type TempLocale = OtherLocales extends ExplicitLocales ? GetLocaleType<OtherLocales> : GetLocaleType<Locales>;
-  type Locale = TempLocale[keyof TempLocale] extends {} ? FlattenLocale<TempLocale> : TempLocale;
+  type Locale = TempLocale extends Record<string, string> ? TempLocale : FlattenLocale<TempLocale>;
 
   type LocalesKeys = OtherLocales extends ExplicitLocales ? keyof OtherLocales : keyof Locales;
 
+  // @ts-expect-error deep type
   const I18nContext = createContext<LocaleContext<Locale> | null>(null);
   const I18nProvider = createI18nProvider(I18nContext, locales);
   const useI18n = createUsei18n(I18nContext);
