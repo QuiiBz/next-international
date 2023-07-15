9428c0265a848608e0642db932071c8e80d8e23e
QuiiBz <tom.lienrd@gmail.com>
2023-07-15 08:12:27 +0200

    fix(next-international): support nested objects in App Router

--- packages/next-international/src/app/server/index.ts
+++ packages/next-international/src/app/server/index.ts
@@ -1,6 +1,6 @@
 import 'server-only';
 
-import type { ExplicitLocales, GetLocaleType, ImportedLocales } from 'international-types';
+import type { ExplicitLocales, FlattenLocale, GetLocaleType, ImportedLocales } from 'international-types';
 import { createGetI18n } from './create-get-i18n';
 import { createGetScopedI18n } from './create-get-scoped-i18n';
 import { createGetCurrentLocale } from './create-get-current-locale';
@@ -9,9 +9,12 @@ import { createGetStaticParams } from './create-get-static-params';
 export function createI18nServer<Locales extends ImportedLocales, OtherLocales extends ExplicitLocales | null = null>(
   locales: Locales,
 ) {
-  type Locale = OtherLocales extends ExplicitLocales ? GetLocaleType<OtherLocales> : GetLocaleType<Locales>;
+  type TempLocale = OtherLocales extends ExplicitLocales ? GetLocaleType<OtherLocales> : GetLocaleType<Locales>;
+  type Locale = TempLocale extends Record<string, string> ? TempLocale : FlattenLocale<TempLocale>;
+
   type LocalesKeys = OtherLocales extends ExplicitLocales ? keyof OtherLocales : keyof Locales;
 
+  // @ts-expect-error deep type
   const getI18n = createGetI18n<Locale>(locales);
   const getScopedI18n = createGetScopedI18n<Locales, Locale>(locales);
   const getCurrentLocale = createGetCurrentLocale<LocalesKeys>();
