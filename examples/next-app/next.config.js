bfd2303d8d434b5ce0b3e27ab87f1c71c846fd68
QuiiBz <tom.lienrd@gmail.com>
2023-08-02 17:54:35 +0200

    fix(next-international): `useChangeLocale` with `basePath`

--- examples/next-app/next.config.js
+++ examples/next-app/next.config.js
@@ -10,6 +10,8 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  // Uncomment to set base path
+  // basePath: '/base',
   // Uncomment to use SSG
   // output: 'export',
 };
