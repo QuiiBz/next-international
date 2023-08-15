c05f4e75f86a05b111fad627ee8df19159bc8d3f
QuiiBz <tom.lienrd@gmail.com>
2023-08-15 11:58:49 +0200

    feat: logo

--- docs/theme.config.jsx
+++ docs/theme.config.jsx
@@ -1,13 +1,20 @@
+import Image from 'next/image';
+
 export default {
-  logo: <strong>next-international</strong>,
+  logo: (
+    <>
+      <Image src="/logo-black.png" alt="next-international logo" height="24" width="24" />
+      <strong style={{ marginLeft: '8px' }}>next-international</strong>
+    </>
+  ),
   project: {
     link: 'https://github.com/QuiiBz/next-international',
   },
   docsRepositoryBase: 'https://github.com/QuiiBz/next-international/blob/main/docs/pages',
   useNextSeoProps() {
     return {
-      titleTemplate: '%s – next-international'
-    }
+      titleTemplate: '%s – next-international',
+    };
   },
   footer: {
     text: (
