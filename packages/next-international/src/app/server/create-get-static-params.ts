3faefa1e24d49bb7a0a7f4d61cc4d644cd43e432
Tom Lienard <tom.lienrd@gmail.com>
2023-08-19 08:42:42 +0200

    feat(next-international): allow configuring segment name in `getStaticParams` (#124)

--- packages/next-international/src/app/server/create-get-static-params.ts
+++ packages/next-international/src/app/server/create-get-static-params.ts
@@ -1,9 +1,11 @@
 import type { ImportedLocales } from 'international-types';
+import { I18nStaticParamsConfig } from '../../types';
+import { DEFAULT_SEGMENT_NAME } from '../../common/constants';
 
 export function createGetStaticParams<Locales extends ImportedLocales>(locales: Locales) {
-  return function getStaticParams() {
+  return function getStaticParams(config?: I18nStaticParamsConfig) {
     return Object.keys(locales).map(locale => ({
-      locale,
+      [config?.segmentName ?? DEFAULT_SEGMENT_NAME]: locale,
     }));
   };
 }
