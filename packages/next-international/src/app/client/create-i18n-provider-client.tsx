a4f130536276af4d0366f384c1b872586d844009
Tom Lienard <tom.lienrd@gmail.com>
2023-08-19 10:06:20 +0200

    feat(next-international): improve config API (#125)

--- packages/next-international/src/app/client/create-i18n-provider-client.tsx
+++ packages/next-international/src/app/client/create-i18n-provider-client.tsx
@@ -1,30 +1,28 @@
 import React, { Context, ReactElement, ReactNode, useCallback, useEffect, useMemo, useState } from 'react';
 import type { BaseLocale, ImportedLocales } from 'international-types';
 
-import type { I18nCurrentLocaleConfig, LocaleContext } from '../../types';
+import type { LocaleContext } from '../../types';
 import { flattenLocale } from '../../common/flatten-locale';
 
 type I18nProviderProps = {
   locale: string;
   fallback?: ReactElement | null;
   fallbackLocale?: Record<string, unknown>;
-  config?: I18nCurrentLocaleConfig;
   children: ReactNode;
 };
 
 export function createI18nProviderClient<Locale extends BaseLocale, LocalesKeys>(
   I18nClientContext: Context<LocaleContext<Locale> | null>,
   locales: ImportedLocales,
-  useCurrentLocale: (config?: I18nCurrentLocaleConfig) => LocalesKeys,
+  useCurrentLocale: () => LocalesKeys,
 ) {
   return function I18nProviderClient({
     locale: baseLocale,
     fallback = null,
     fallbackLocale,
     children,
-    config,
   }: I18nProviderProps) {
-    const locale = useCurrentLocale(config);
+    const locale = useCurrentLocale();
     const [clientLocale, setClientLocale] = useState<Locale>();
 
     const loadLocale = useCallback((locale: string) => {
