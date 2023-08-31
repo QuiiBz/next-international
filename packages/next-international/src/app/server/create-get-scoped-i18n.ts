748cc7458576e8d9b247ead2641a25352fa1a215
QuiiBz <tom.lienrd@gmail.com>
2023-08-31 18:37:14 +0200

    feat(next-international): improve fallback locale

--- packages/next-international/src/app/server/create-get-scoped-i18n.ts
+++ packages/next-international/src/app/server/create-get-scoped-i18n.ts
@@ -1,17 +1,22 @@
 import type { BaseLocale, ImportedLocales, Scopes } from 'international-types';
 import { createT } from '../../common/create-t';
-import { LocaleContext } from '../../types';
+import { I18nServerConfig, LocaleContext } from '../../types';
 import { getLocaleCache } from './get-locale-cache';
 import { flattenLocale } from '../../common/flatten-locale';
 
-export function createGetScopedI18n<Locales extends ImportedLocales, Locale extends BaseLocale>(locales: Locales) {
+export function createGetScopedI18n<Locales extends ImportedLocales, Locale extends BaseLocale>(
+  locales: Locales,
+  config: I18nServerConfig<keyof Locales>,
+) {
   return async function getScopedI18n<Scope extends Scopes<Locale>>(scope: Scope) {
     const locale = getLocaleCache();
 
     return createT(
       {
         localeContent: flattenLocale((await locales[locale]()).default),
-        fallbackLocale: undefined,
+        fallbackLocale: config.fallbackLocale
+          ? flattenLocale((await locales[config.fallbackLocale]()).default)
+          : undefined,
         locale,
       } as LocaleContext<Locale>,
       scope,
