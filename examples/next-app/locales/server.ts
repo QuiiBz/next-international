b6bf3ef862e2e4f054bf304b17805f5cd4841d3a
Tom Lienard <tom.lienrd@gmail.com>
2023-09-02 19:05:56 +0200

    refactor(next-international): load fallback locale immediately (#150)

--- examples/next-app/locales/server.ts
+++ examples/next-app/locales/server.ts
@@ -1,4 +1,5 @@
 import { createI18nServer } from 'next-international/server';
+// import en from './en';
 
 export const { getI18n, getScopedI18n, getCurrentLocale, getStaticParams } = createI18nServer(
   {
@@ -9,6 +10,6 @@ export const { getI18n, getScopedI18n, getCurrentLocale, getStaticParams } = cre
     // Uncomment to use custom segment name
     // segmentName: 'locale',
     // Uncomment to set fallback locale
-    // fallbackLocale: 'en',
+    // fallbackLocale: en,
   },
 );
