ee6848dc8915446fd14a96e26377afed6d32bae3
QuiiBz <tom.lienrd@gmail.com>
2023-09-08 09:44:57 +0200

    fix: rename

--- examples/next-app/app/[locale]/page.tsx
+++ examples/next-app/app/[locale]/page.tsx
@@ -7,20 +7,20 @@ import { getI18n, getScopedI18n, getCurrentLocale } from '../../locales/server';
 // }
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-export default async function Home({ params: { locale: l } }: { params: { locale: string } }) {
+export default async function Home({ params: { locale } }: { params: { locale: string } }) {
   // Uncomment to test Static Generation
-  // setStaticParamsLocale(l);
+  // setStaticParamsLocale(locale);
 
   const t = await getI18n();
   const t2 = await getScopedI18n('scope.more');
-  const locale = getCurrentLocale();
+  const currentLocale = getCurrentLocale();
 
   return (
     <div>
       <h1>SSR / SSG</h1>
       <p>
         Current locale:
-        <span>{locale}</span>
+        <span>{currentLocale}</span>
       </p>
       <p>Hello: {t('hello')}</p>
       <p>
