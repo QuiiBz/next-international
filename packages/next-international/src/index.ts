62fe48b48f423d6183e4d41ea863a37fe7fe48d2
Tom Lienard <tom.lienrd@gmail.com>
2023-10-08 09:06:48 +0100

    feat(next-international): export internal functions (#232)

--- packages/next-international/src/index.ts
+++ packages/next-international/src/index.ts
@@ -1,3 +1,5 @@
 export { createI18n } from './pages';
+export { createT } from './common/create-t';
+export { flattenLocale } from './common/flatten-locale';
 export type { ReactParamsObject } from './types';
 export type { BaseLocale, LocaleValue } from 'international-types';
