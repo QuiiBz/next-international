69d0bede0edb25400c7e49d25c48c01b667676c1
Willem-Jaap <willemjaapk@gmail.com>
2023-10-12 23:29:32 +0200

    chore: remove @svgr/webpack config in next config

--- docs/next.config.js
+++ docs/next.config.js
@@ -1,19 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  webpack(config) {
-    const allowedSvgRegex = /components\/icons\/.+\.svg$/;
-
-    const fileLoaderRule = config.module.rules.find(rule => rule.test?.test('.svg'));
-    fileLoaderRule.exclude = allowedSvgRegex;
-
-    config.module.rules.push({
-      test: allowedSvgRegex,
-      use: ['@svgr/webpack'],
-    });
-
-    return config;
-  },
 };
 
 const withNextra = require('nextra')({
