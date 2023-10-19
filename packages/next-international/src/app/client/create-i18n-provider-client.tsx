1bb5c4f7ca37bb3ed6c334d1fbe55ce746555ef0
ichiwa <shimokawa@devalon.biz>
2023-10-19 14:12:01 +0900

    refactor: Update error message for clarity on defined locales

--- packages/next-international/src/app/client/create-i18n-provider-client.tsx
+++ packages/next-international/src/app/client/create-i18n-provider-client.tsx
@@ -3,17 +3,13 @@ import type { Context, ReactNode } from 'react';
 import React, { Suspense, use, useMemo } from 'react';
 import { flattenLocale } from '../../common/flatten-locale';
 import type { LocaleContext } from '../../types';
-
 type I18nProviderProps = Omit<I18nProviderWrapperProps, 'fallback'>;
-
 type I18nProviderWrapperProps = {
   locale: string;
   fallback?: ReactNode;
   children: ReactNode;
 };
-
 export const localesCache = new Map<string, Record<string, unknown>>();
-
 export function createI18nProviderClient<Locale extends BaseLocale>(
   I18nClientContext: Context<LocaleContext<Locale> | null>,
   locales: ImportedLocales,
@@ -21,12 +17,16 @@ export function createI18nProviderClient<Locale extends BaseLocale>(
 ) {
   function I18nProvider({ locale, children }: I18nProviderProps) {
     let clientLocale: any = localesCache.get(locale);
-
     if (!clientLocale) {
-      clientLocale = use(locales[locale as keyof typeof locales]()).default;
+      const newLocale = locales[locale as keyof typeof locales];
+      if (!newLocale) {
+        throw new Error(
+          `The locale '${locale}' is not supported. Defined locales are: [${Object.keys(locales).join(', ')}].`,
+        );
+      }
+      clientLocale = use(newLocale()).default;
       localesCache.set(locale, clientLocale);
     }
-
     const value = useMemo(
       () => ({
         localeContent: flattenLocale<Locale>(clientLocale),
@@ -35,10 +35,8 @@ export function createI18nProviderClient<Locale extends BaseLocale>(
       }),
       [clientLocale, locale],
     );
-
     return <I18nClientContext.Provider value={value}>{children}</I18nClientContext.Provider>;
   }
-
   return function I18nProviderWrapper({ locale, fallback, children }: I18nProviderWrapperProps) {
     return (
       <Suspense fallback={fallback}>
