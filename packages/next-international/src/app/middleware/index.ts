ec618d13df73036d5487cfbfc695e0999b7027cc
Quentin <contact@yovach.dev>
2023-10-27 22:27:33 +0200

    fix(lint): format code to follow prettier rules

--- packages/next-international/src/app/middleware/index.ts
+++ packages/next-international/src/app/middleware/index.ts
@@ -102,7 +102,7 @@ function noLocalePrefix(locales: readonly string[], pathname: string) {
 
 /**
  * Add `X-Next-Locale` header and `Next-Locale` cookie to response
- * 
+ *
  * **NOTE:** The cookie is only set if the locale is different from the one in the cookie
  */
 function addLocaleToResponse(request: NextRequest, response: NextResponse, locale: string) {
