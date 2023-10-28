ad9c30d9c99760b64549ec7eec66fbefdc6b99e8
Quentin <contact@yovach.dev>
2023-10-28 09:54:41 +0200

    fix(next-international): preserve cookies in the middleware (#271)

--- packages/next-international/src/app/middleware/index.ts
+++ packages/next-international/src/app/middleware/index.ts
@@ -2,8 +2,8 @@ import type { NextRequest } from 'next/server';
 import { NextResponse } from 'next/server';
 
 import { LOCALE_COOKIE, LOCALE_HEADER } from '../../common/constants';
-import type { I18nMiddlewareConfig } from '../../types';
 import { warn } from '../../helpers/log';
+import type { I18nMiddlewareConfig } from '../../types';
 
 const DEFAULT_STRATEGY: NonNullable<I18nMiddlewareConfig<[]>['urlMappingStrategy']> = 'redirect';
 
@@ -13,21 +13,19 @@ export function createI18nMiddleware<const Locales extends readonly string[]>(co
     const locale = localeFromRequest(config.locales, request, config.resolveLocaleFromRequest) ?? config.defaultLocale;
 
     if (noLocalePrefix(config.locales, requestUrl.pathname)) {
-      const mappedUrl = requestUrl.clone();
-      mappedUrl.pathname = `/${locale}${mappedUrl.pathname}`;
+      requestUrl.pathname = `/${locale}${requestUrl.pathname}`;
 
       const strategy = config.urlMappingStrategy ?? DEFAULT_STRATEGY;
-
       if (strategy === 'rewrite' || (strategy === 'rewriteDefault' && locale === config.defaultLocale)) {
-        const response = NextResponse.rewrite(mappedUrl);
-        return addLocaleToResponse(response, locale);
+        const response = NextResponse.rewrite(requestUrl);
+        return addLocaleToResponse(request, response, locale);
       } else {
         if (!['redirect', 'rewriteDefault'].includes(strategy)) {
           warn(`Invalid urlMappingStrategy: ${strategy}. Defaulting to redirect.`);
         }
 
-        const response = NextResponse.redirect(mappedUrl);
-        return addLocaleToResponse(response, locale);
+        const response = NextResponse.redirect(requestUrl);
+        return addLocaleToResponse(request, response, locale);
       }
     }
 
@@ -52,13 +50,17 @@ export function createI18nMiddleware<const Locales extends readonly string[]>(co
         response = NextResponse.redirect(newUrl);
       }
 
-      return addLocaleToResponse(response, requestLocale ?? config.defaultLocale);
+      return addLocaleToResponse(request, response, requestLocale ?? config.defaultLocale);
     }
 
     return response;
   };
 }
 
+/**
+ * Retrieve `Next-Locale` header from request
+ * and check if it is an handled locale.
+ */
 function localeFromRequest<Locales extends readonly string[]>(
   locales: Locales,
   request: NextRequest,
@@ -79,20 +81,35 @@ function localeFromRequest<Locales extends readonly string[]>(
   return locale;
 }
 
+/**
+ * Default implementation of the `resolveLocaleFromRequest` function for the I18nMiddlewareConfig.
+ * This function extracts the locale from the 'Accept-Language' header of the request.
+ */
 const defaultResolveLocaleFromRequest: NonNullable<I18nMiddlewareConfig<any>['resolveLocaleFromRequest']> = request => {
   const header = request.headers.get('Accept-Language');
   const locale = header?.split(',', 1)?.[0]?.split('-', 1)?.[0];
   return locale ?? null;
 };
 
+/**
+ * Returns `true` if the pathname does not start with an handled locale
+ */
 function noLocalePrefix(locales: readonly string[], pathname: string) {
   return locales.every(locale => {
     return !(pathname === `/${locale}` || pathname.startsWith(`/${locale}/`));
   });
 }
 
-function addLocaleToResponse(response: NextResponse, locale: string) {
+/**
+ * Add `X-Next-Locale` header and `Next-Locale` cookie to response
+ *
+ * **NOTE:** The cookie is only set if the locale is different from the one in the cookie
+ */
+function addLocaleToResponse(request: NextRequest, response: NextResponse, locale: string) {
   response.headers.set(LOCALE_HEADER, locale);
-  response.cookies.set(LOCALE_COOKIE, locale, { sameSite: 'strict' });
+
+  if (request.cookies.get(LOCALE_COOKIE)?.value !== locale) {
+    response.cookies.set(LOCALE_COOKIE, locale, { sameSite: 'strict' });
+  }
   return response;
 }
