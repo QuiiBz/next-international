0bdc9886631d0f903b52c5223310db5dae764a1a
Tom Lienard <tom.lienrd@gmail.com>
2023-12-19 20:30:37 +0100

    feat: test static build

--- examples/next-app/app/[locale]/subpage/page.tsx
+++ examples/next-app/app/[locale]/subpage/page.tsx
@@ -1,15 +1,15 @@
-// import { setStaticParamsLocale } from 'next-international/server';
-import { getI18n } from '../../../locales/server';
+import { setStaticParamsLocale } from 'next-international/server';
+import { getI18n, getStaticParams } from '../../../locales/server';
 
 // Uncomment to test Static Generation on this page only
-// export function generateStaticParams() {
-//   return getStaticParams();
-// }
+export function generateStaticParams() {
+  return getStaticParams();
+}
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export default async function Subpage({ params: { locale } }: { params: { locale: string } }) {
   // Uncomment to test Static Generation
-  // setStaticParamsLocale(locale);
+  setStaticParamsLocale(locale);
 
   const t = await getI18n();
 
