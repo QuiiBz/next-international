637c2f95012ba614d2c0807ab059937eaed18371
Tom Lienard <tom.lienrd@gmail.com>
2024-02-18 09:15:01 +0000

    feat(v2): not found catch-all (#363)

--- examples/app/app/[locale]/layout.tsx
+++ examples/app/app/[locale]/layout.tsx
@@ -1,18 +1,15 @@
 import { I18nProvider, generateI18nStaticParams } from '@/locales';
 import React from 'react';
 
-export const generateStaticParams = generateI18nStaticParams();
+export const dynamicParams = false;
+export function generateStaticParams() {
+  return generateI18nStaticParams();
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body>
-        <I18nProvider>{children}</I18nProvider>
-      </body>
-    </html>
-  );
+  return <I18nProvider>{children}</I18nProvider>;
 }
